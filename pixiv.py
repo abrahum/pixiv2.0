b2d90483aa99507e83c64458a5a0ff9fa211dd3a
link <307887491@qq.com>
2017-02-07 09:04:18 +0800

    test for linux

--- pixiv.py
+++ pixiv.py
@@ -12,6 +12,7 @@ import painter
 import threading
 import tqdm
 import getopt
+import web
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML,\
  like Gecko) Chrome/50.0.2661.102 Safari/537.36'
